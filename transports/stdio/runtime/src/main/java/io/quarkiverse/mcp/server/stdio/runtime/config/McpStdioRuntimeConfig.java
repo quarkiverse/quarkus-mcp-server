9a5194c578d224f2c988d1a21608ba7debffb466
Martin Kouba <mkouba@redhat.com>
2025-01-29 16:34:53 +0100

    stdio: make it possible to disable the transport completely

--- transports/stdio/runtime/src/main/java/io/quarkiverse/mcp/server/stdio/runtime/config/McpStdioRuntimeConfig.java
+++ transports/stdio/runtime/src/main/java/io/quarkiverse/mcp/server/stdio/runtime/config/McpStdioRuntimeConfig.java
@@ -10,6 +10,18 @@ import io.smallrye.config.WithDefault;
 public interface McpStdioRuntimeConfig {
 
     /**
+     * If set to `false` then the stdio transport is completely disabled, i.e. the application does not read/write messages
+     * from/to the standard input/output.
+     *
+     * Keep in mind that console logging is still automatically redirected to the standard error. You will need to set the
+     * `quarkus.log.console.stderr` to `false` to suppress this behavior.
+     *
+     * @asciidoclet
+     */
+    @WithDefault("true")
+    boolean enabled();
+
+    /**
      * If set to `true` then the standard output stream is set to "null" when the app is started.
      *
      * @asciidoclet
