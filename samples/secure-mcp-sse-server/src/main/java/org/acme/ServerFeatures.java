0b5600eed7bd1c5bfb1875ae25665533acc2a52b
Sergey Beryozkin <sberyozkin@gmail.com>
2025-04-09 15:31:25 +0100

    Add secure-mcp-sse-server demo

---
+++ samples/secure-mcp-sse-server/src/main/java/org/acme/ServerFeatures.java
@@ -0,0 +1,19 @@
+package org.acme;
+
+import io.quarkiverse.mcp.server.TextContent;
+import io.quarkiverse.mcp.server.Tool;
+import io.quarkus.security.Authenticated;
+import io.quarkus.security.identity.SecurityIdentity;
+import jakarta.inject.Inject;
+
+public class ServerFeatures {
+
+    @Inject
+    SecurityIdentity identity;
+
+    @Authenticated
+    @Tool(name = "user-name-provider", description = "Provides a name of the current user")
+    TextContent provideUserName() {
+        return new TextContent(identity.getPrincipal().getName());
+    }
+}
