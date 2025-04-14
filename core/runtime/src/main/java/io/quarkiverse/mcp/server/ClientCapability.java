169caaad848dfc02525848d3846c53a37634e193
Martin Kouba <mkouba@redhat.com>
2025-04-14 13:44:02 +0200

    Fix ClientCapability#SAMPLING constant

--- core/runtime/src/main/java/io/quarkiverse/mcp/server/ClientCapability.java
+++ core/runtime/src/main/java/io/quarkiverse/mcp/server/ClientCapability.java
@@ -9,6 +9,6 @@ public record ClientCapability(String name, Map<String, Object> properties) {
 
     public static final String ROOTS = "roots";
 
-    public static final String SAMPLING = "sampling ";
+    public static final String SAMPLING = "sampling";
 
 }
