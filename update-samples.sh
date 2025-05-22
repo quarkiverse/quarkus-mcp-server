5a2ae921de6af236fa89a1e614febc2dec6edfe8
Martin Kouba <mkouba@redhat.com>
2025-05-22 17:28:14 +0200

    samples: update MCP server version, add CI build

---
+++ update-samples.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+./samples/weather/mvnw versions:use-dep-version -Dincludes=io.quarkiverse.mcp:quarkus-mcp-server-stdio -DdepVersion=${CURRENT_VERSION} -DgenerateBackupPoms=false -f samples/weather/pom.xml 
+./samples/secure-mcp-sse-server/mvnw versions:use-dep-version -Dincludes=io.quarkiverse.mcp:quarkus-mcp-server-sse -DdepVersion=${CURRENT_VERSION} -DgenerateBackupPoms=false -f samples/secure-mcp-sse-server/pom.xml 
