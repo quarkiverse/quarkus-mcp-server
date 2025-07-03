a12b84ca563646b749152428d3476c2c1516e326
Martin Kouba <mkouba@redhat.com>
2025-07-03 15:56:08 +0200

    sse: return HTTP 405 if POST is sent to the legacy SSE endpoint
    - fixes #317

---
+++ transports/sse/deployment/src/test/java/io/quarkiverse/mcp/server/test/streamablehttp/InitPostAttemptTest.java
@@ -0,0 +1,51 @@
+package io.quarkiverse.mcp.server.test.streamablehttp;
+
+import java.net.URI;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkiverse.mcp.server.Tool;
+import io.quarkiverse.mcp.server.test.McpAssured;
+import io.quarkiverse.mcp.server.test.McpAssured.McpSseTestClient;
+import io.quarkiverse.mcp.server.test.McpServerTest;
+import io.quarkus.test.QuarkusUnitTest;
+import io.restassured.RestAssured;
+import io.vertx.core.json.JsonObject;
+
+public class InitPostAttemptTest extends McpServerTest {
+
+    @RegisterExtension
+    static final QuarkusUnitTest config = defaultConfig()
+            .withApplicationRoot(
+                    root -> root.addClasses(MyTools.class));
+
+    @Test
+    public void testFailures() {
+        McpSseTestClient client = McpAssured.newConnectedSseClient();
+        URI sseEndpoint = client.sseEndpoint();
+        JsonObject initMessage = client.newMessage("initialize");
+        JsonObject params = new JsonObject()
+                .put("clientInfo", new JsonObject()
+                        .put("name", "test")
+                        .put("version", "1.0"))
+                .put("protocolVersion", "2024-11-05");
+        initMessage.put("params", params);
+        client.disconnect();
+        RestAssured.given()
+                .when()
+                .body(initMessage.encode())
+                .post(sseEndpoint)
+                .then()
+                .statusCode(405);
+    }
+
+    public static class MyTools {
+
+        @Tool
+        String bravo(int price) {
+            return "" + price * 42;
+        }
+    }
+
+}
