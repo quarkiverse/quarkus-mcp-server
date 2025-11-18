28f557d80d3aad1c87a7d00920ade9a692fe3a4b
Martin Kouba <mkouba@redhat.com>
2025-11-18 19:55:31 +0100

    http: make transport compatible with Quarkus 3.30+
    - fixes #491

--- transports/sse/deployment/src/main/java/io/quarkiverse/mcp/server/sse/deployment/SseMcpServerProcessor.java
+++ transports/sse/deployment/src/main/java/io/quarkiverse/mcp/server/sse/deployment/SseMcpServerProcessor.java
@@ -1,6 +1,7 @@
 package io.quarkiverse.mcp.server.sse.deployment;
 
 import static io.quarkus.deployment.annotations.ExecutionTime.RUNTIME_INIT;
+import static io.quarkus.deployment.annotations.ExecutionTime.STATIC_INIT;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -8,37 +9,28 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import jakarta.enterprise.inject.spi.EventContext;
-
-import org.jboss.jandex.DotName;
+import jakarta.inject.Singleton;
 
 import io.quarkiverse.mcp.server.deployment.ServerNameBuildItem;
+import io.quarkiverse.mcp.server.sse.runtime.McpServerEndpoints;
+import io.quarkiverse.mcp.server.sse.runtime.McpServerEndpoints.McpServerEndpoint;
+import io.quarkiverse.mcp.server.sse.runtime.McpServerEndpointsLogger;
 import io.quarkiverse.mcp.server.sse.runtime.SseMcpMessageHandler;
 import io.quarkiverse.mcp.server.sse.runtime.SseMcpServerRecorder;
-import io.quarkiverse.mcp.server.sse.runtime.SseMcpServerRecorder.McpServerEndpoints;
 import io.quarkiverse.mcp.server.sse.runtime.StreamableHttpMcpMessageHandler;
 import io.quarkiverse.mcp.server.sse.runtime.config.McpSseServerBuildTimeConfig;
 import io.quarkiverse.mcp.server.sse.runtime.config.McpSseServersBuildTimeConfig;
 import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
-import io.quarkus.arc.deployment.ObserverRegistrationPhaseBuildItem;
-import io.quarkus.arc.deployment.ObserverRegistrationPhaseBuildItem.ObserverConfiguratorBuildItem;
+import io.quarkus.arc.deployment.SyntheticBeanBuildItem;
 import io.quarkus.arc.deployment.SyntheticBeansRuntimeInitBuildItem;
-import io.quarkus.arc.processor.ObserverConfigurator;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.annotations.Consume;
 import io.quarkus.deployment.annotations.Record;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
-import io.quarkus.gizmo.Gizmo;
-import io.quarkus.gizmo.MethodCreator;
-import io.quarkus.gizmo.MethodDescriptor;
-import io.quarkus.gizmo.ResultHandle;
-import io.quarkus.vertx.http.HttpServerStart;
-import io.quarkus.vertx.http.HttpsServerStart;
 import io.quarkus.vertx.http.deployment.BodyHandlerBuildItem;
 import io.quarkus.vertx.http.deployment.HttpRootPathBuildItem;
 import io.quarkus.vertx.http.deployment.spi.RouteBuildItem;
-import io.vertx.core.http.HttpServerOptions;
 
 public class SseMcpServerProcessor {
 
@@ -50,8 +42,11 @@ public class SseMcpServerProcessor {
     @BuildStep
     void addBeans(BuildProducer<AdditionalBeanBuildItem> additionalBeans) {
         additionalBeans.produce(
-                AdditionalBeanBuildItem.builder().setUnremovable()
-                        .addBeanClasses(SseMcpMessageHandler.class, StreamableHttpMcpMessageHandler.class).build());
+                AdditionalBeanBuildItem.builder()
+                        .setUnremovable()
+                        .addBeanClasses(SseMcpMessageHandler.class, StreamableHttpMcpMessageHandler.class,
+                                McpServerEndpointsLogger.class)
+                        .build());
     }
 
     @BuildStep
@@ -64,79 +59,55 @@ public class SseMcpServerProcessor {
     @Record(RUNTIME_INIT)
     @Consume(SyntheticBeansRuntimeInitBuildItem.class)
     @BuildStep
-    void registerEndpoints(McpSseServersBuildTimeConfig config, HttpRootPathBuildItem httpRootPath,
+    void registerEndpoints(McpServerEndpointsBuildItem mcpServerEndpoints,
             SseMcpServerRecorder recorder,
             BodyHandlerBuildItem bodyHandler,
-            BuildProducer<RouteBuildItem> routes,
-            ObserverRegistrationPhaseBuildItem observerRegistrationPhase,
-            BuildProducer<ObserverConfiguratorBuildItem> observers) {
+            BuildProducer<RouteBuildItem> routes) {
+        for (McpServerEndpoint endpoint : mcpServerEndpoints.getEndpoints()) {
+            // Streamable HTTP transport
+            routes.produce(RouteBuildItem.newFrameworkRoute(endpoint.mcpPath)
+                    .withRouteCustomizer(recorder.addBodyHandler(bodyHandler.getHandler()))
+                    .withRequestHandler(recorder.createMcpEndpointHandler(endpoint.serverName))
+                    .build());
+            // SSE/HTTP transport
+            routes.produce(RouteBuildItem.newFrameworkRoute(endpoint.ssePath)
+                    .withRequestHandler(recorder.createSseEndpointHandler(endpoint.mcpPath, endpoint.serverName))
+                    .build());
+            routes.produce(RouteBuildItem.newFrameworkRoute(endpoint.mcpPath + "/" + "messages/:id")
+                    .withRouteCustomizer(recorder.addBodyHandler(bodyHandler.getHandler()))
+                    .withRequestHandler(recorder.createMessagesEndpointHandler(endpoint.serverName))
+                    .build());
+        }
+    }
 
+    @Record(STATIC_INIT)
+    @BuildStep
+    void registerMcpEndpointsBean(McpServerEndpointsBuildItem mcpServerEndpoints, SseMcpServerRecorder recorder,
+            BuildProducer<SyntheticBeanBuildItem> syntheticBeans) {
+        syntheticBeans.produce(SyntheticBeanBuildItem.configure(McpServerEndpoints.class)
+                .scope(Singleton.class)
+                .createWith(recorder.createMcpServerEndpoints(mcpServerEndpoints.getEndpoints()))
+                .done());
+    }
+
+    @BuildStep
+    McpServerEndpointsBuildItem collectMcpServerEndpoints(McpSseServersBuildTimeConfig config,
+            HttpRootPathBuildItem httpRootPath) {
+        List<McpServerEndpoint> endpoints = new ArrayList<>();
         Set<String> rootPaths = new HashSet<>();
-        List<McpServerEndpoints> endpoints = new ArrayList<>();
         for (Map.Entry<String, McpSseServerBuildTimeConfig> e : config.servers().entrySet()) {
             String serverName = e.getKey();
             String rootPath = e.getValue().sse().rootPath();
             if (!rootPaths.add(rootPath)) {
                 throw new IllegalStateException("Multiple server configurations define the same root path: " + rootPath);
             }
-
-            // By default /mcp
+            // Streamable HTTP transport, by default /mcp
             String mcpPath = httpRootPath.relativePath(rootPath);
-
-            // Streamable HTTP transport
-            routes.produce(RouteBuildItem.newFrameworkRoute(mcpPath)
-                    .withRouteCustomizer(recorder.addBodyHandler(bodyHandler.getHandler()))
-                    .withRequestHandler(recorder.createMcpEndpointHandler(serverName))
-                    .build());
-
-            // SSE/HTTP transport
+            // SSE/HTTP transport, by default /mcp/sse
             String ssePath = mcpPath.endsWith("/") ? mcpPath + "sse" : mcpPath + "/sse";
-            routes.produce(RouteBuildItem.newFrameworkRoute(ssePath)
-                    .withRequestHandler(recorder.createSseEndpointHandler(mcpPath, serverName))
-                    .build());
-            routes.produce(RouteBuildItem.newFrameworkRoute(mcpPath + "/" + "messages/:id")
-                    .withRouteCustomizer(recorder.addBodyHandler(bodyHandler.getHandler()))
-                    .withRequestHandler(recorder.createMessagesEndpointHandler(serverName))
-                    .build());
-
-            endpoints.add(new McpServerEndpoints(serverName, mcpPath, ssePath));
+            endpoints.add(new McpServerEndpoint(serverName, mcpPath, ssePath));
         }
-
-        // Create synthetic observers for HttpServerStart and HttpsServerStart
-        // so that the info is logged after the server is started
-        ObserverConfigurator httpStartConfigurator = observerRegistrationPhase.getContext()
-                .configure()
-                .async(true)
-                .beanClass(DotName.createSimple(SseMcpServerRecorder.class))
-                .observedType(HttpServerStart.class)
-                .notify(mc -> logMcpServerEndpoints(HttpServerStart.class, mc, endpoints));
-        ObserverConfigurator httpsStartConfigurator = observerRegistrationPhase.getContext()
-                .configure()
-                .async(true)
-                .beanClass(DotName.createSimple(SseMcpServerRecorder.class))
-                .observedType(HttpsServerStart.class)
-                .notify(mc -> logMcpServerEndpoints(HttpsServerStart.class, mc, endpoints));
-        observers.produce(new ObserverConfiguratorBuildItem(httpStartConfigurator, httpsStartConfigurator));
-    }
-
-    private void logMcpServerEndpoints(Class<?> eventType, MethodCreator mc, List<McpServerEndpoints> endpoints) {
-        ResultHandle event = mc.invokeInterfaceMethod(MethodDescriptor.ofMethod(EventContext.class, "getEvent", Object.class),
-                mc.getMethodParam(0));
-        ResultHandle httpServerOptions = mc.invokeVirtualMethod(
-                MethodDescriptor.ofMethod(eventType, "options", HttpServerOptions.class),
-                event);
-        ResultHandle list = Gizmo.newArrayList(mc);
-        for (McpServerEndpoints e : endpoints) {
-            ResultHandle mcpe = mc.newInstance(MethodDescriptor.ofConstructor(McpServerEndpoints.class,
-                    String.class, String.class, String.class), mc.load(e.serverName), mc.load(e.mcpPath),
-                    mc.load(e.ssePath));
-            Gizmo.listOperations(mc).on(list).add(mcpe);
-        }
-        mc.invokeStaticMethod(
-                MethodDescriptor.ofMethod(SseMcpServerRecorder.class, "logEndpoints", void.class, List.class,
-                        HttpServerOptions.class),
-                list, httpServerOptions);
-        mc.returnVoid();
+        return new McpServerEndpointsBuildItem(endpoints);
     }
 
 }
---
+++ transports/sse/runtime/src/main/java/io/quarkiverse/mcp/server/sse/runtime/McpServerEndpoints.java
@@ -0,0 +1,27 @@
+package io.quarkiverse.mcp.server.sse.runtime;
+
+import java.util.List;
+
+public class McpServerEndpoints {
+
+    final List<McpServerEndpoints.McpServerEndpoint> endpoints;
+
+    McpServerEndpoints(List<McpServerEndpoint> endpoints) {
+        this.endpoints = endpoints;
+    }
+
+    public static class McpServerEndpoint {
+
+        public String serverName;
+        public String mcpPath;
+        public String ssePath;
+
+        public McpServerEndpoint(String serverName, String mcpPath, String ssePath) {
+            this.serverName = serverName;
+            this.mcpPath = mcpPath;
+            this.ssePath = ssePath;
+        }
+
+    }
+
+}
