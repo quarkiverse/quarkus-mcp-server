360951b981d3975d0319c6ecb8cc71bc921fdbf4
Martin Kouba <mkouba@redhat.com>
2026-02-25 15:50:25 +0100

    websocket: support multiple MCP server configurations

---
+++ transports/websocket/deployment/src/test/java/io/quarkiverse/mcp/server/websocket/test/mcpservers/InvalidServerNameTest.java
@@ -0,0 +1,34 @@
+package io.quarkiverse.mcp.server.websocket.test.mcpservers;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkiverse.mcp.server.McpServer;
+import io.quarkiverse.mcp.server.Tool;
+import io.quarkus.test.QuarkusUnitTest;
+
+public class InvalidServerNameTest {
+
+    @RegisterExtension
+    static final QuarkusUnitTest config = new QuarkusUnitTest()
+            .withApplicationRoot(root -> root.addClasses(MyTools.class))
+            .setExpectedException(IllegalStateException.class, true);
+
+    @Test
+    public void test() {
+        fail();
+    }
+
+    public static class MyTools {
+
+        @Tool
+        @McpServer("bravo")
+        String bravo() {
+            return "2";
+        }
+
+    }
+
+}
