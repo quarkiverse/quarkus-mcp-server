74c55b74b4d301fa1b1cc4ace7f3299bef84351e
Martin Kouba <mkouba@redhat.com>
2026-05-20 17:38:17 +0200

    http: update auto-init docs

--- transports/http/runtime/src/main/java/io/quarkiverse/mcp/server/http/runtime/config/McpHttpServerRuntimeConfig.java
+++ transports/http/runtime/src/main/java/io/quarkiverse/mcp/server/http/runtime/config/McpHttpServerRuntimeConfig.java
@@ -28,8 +28,11 @@ public interface McpHttpServerRuntimeConfig {
 
             /**
              * If set to `true` then the server performs an automatic initialization when the first message from the client is
-             * not `initialize`. A new MCP session is created for each request. The capability negotiation and protocol version
-             * agreement is completely skipped, so the auto-initialized client supports no capabilities.
+             * not `initialize`. A new MCP session is created for each request. The formal capability negotiation and protocol
+             * version agreement is skipped. However, the client can optionally provide its info and capabilities through the
+             * `_meta` fields of the request params: `io.modelcontextprotocol/clientInfo` and
+             * `io.modelcontextprotocol/clientCapabilities`. The protocol version can be specified with the
+             * `Mcp-Protocol-Version` header.
              *
              * Auto-initialization can be used to simulate stateless communication. However, it's not efficient and some
              * features may not work properly.
